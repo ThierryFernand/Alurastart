c95f55a0917d956690384e731e352468dec2aeb9
ThierryFernand <thierryfernando42@gmail.com>
2024-09-06 20:01:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,46 @@
+
+let Nikola_Tesla = {
+    título: 'Nikola Tesla',
+    descrição: ' Nikola Tesla foi um inventor sérvio que revolucionou o mundo da eletricidade. Suas maiores contribuições incluem a criação do sistema de corrente alternada, que usamos até hoje, e a invenção da bobina de Tesla, que gera descargas elétricas espetaculares. Apesar de suas muitas invenções, Tesla enfrentou desafios e não sempre recebeu o reconhecimento que merecia. Suas ideias avançadas, como a transmissão de energia sem fio, continuam a inspirar cientistas e engenheiros até os dias de hoje.',
+    link: 'https://pt.wikipedia.org/wiki/Nikola_Tesla',
+    tags: 'corrente alternada bobina de Tesla energia livre invenções futurologia Guerra das Correntes '
+}
+let Albert = {
+    título: 'Albert Einsten',
+    descrição: 'Albert Einstein foi um físico teórico alemão que desenvolveu a teoria da relatividade, uma das mais importantes teorias científicas do século XX. Seus trabalhos sobre a natureza da luz, o espaço, o tempo e a gravidade revolucionaram a física e a nossa compreensão do universo.',
+    link: 'https://pt.wikipedia.org/wiki/Albert_Einstein',
+    tags: `teoria da relatividade
+energia
+massa
+espaço-tempo
+física quântica
+E=mc²`
+}
+let Marie = {
+    título: 'Marie Curie',
+    descrição: ' Marie Curie foi uma física e química polonesa naturalizada francesa, pioneira no estudo da radioatividade. Ela foi a primeira mulher a ganhar um Prêmio Nobel, e a única a ganhar em duas áreas diferentes: Física e Química. Suas pesquisas tiveram um impacto profundo na medicina e na física nuclear.',
+    link: 'https://pt.wikipedia.org/wiki/Marie_Curie',
+    tags: `radioatividade
+rádio
+polônio
+Nobel
+ciência feminina
+elementos químico`
+}
+let Issac = {
+    título: 'Isaac Newton',
+    descrição: 'Isaac Newton foi um físico, matemático, astrônomo, filósofo natural e alquimista inglês. Ele é mais conhecido por formular a lei da gravitação universal e as leis do movimento, que formaram a base da mecânica clássica e dominaram a visão científica do universo durante três séculos.',
+    link: 'https://pt.wikipedia.org/wiki/Isaac_Newton',
+    tags: `leis de Newton
+gravitação universal
+cálculo
+física clássica
+ótica
+mecânica`
+}
+let dados = [
+    Nikola_Tesla,
+    Albert,
+    Marie,
+    Issac
+]; 
